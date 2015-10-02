f80a5b515fdcc46dec7ca92091d5e065d8c591bf
amirsad2 <deadamirbot@gmail.com>
2015-10-02 12:31:29 +0330

    Update groupmanager.lua

--- plugins/groupmanager.lua
+++ plugins/groupmanager.lua
@@ -4,9 +4,7 @@ do
 
 local function create_group(msg)
     -- superuser and admins only (because sudo are always has privilege)
-    if not is_admin(msg) then
-        return "You're not admin!"
-    end
+    
     local group_creator = msg.from.print_name
     create_group_chat (group_creator, group_name, ok_cb, false)
 	return 'Group '..string.gsub(group_name, '_', ' ')..' has been created.'
