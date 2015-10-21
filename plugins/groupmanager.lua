6210bbeddefb67bf19f26d5d20bfff4de3117baf
amirsad2 <deadamirbot@gmail.com>
2015-10-21 14:41:01 +0330

    Update groupmanager.lua

--- plugins/groupmanager.lua
+++ plugins/groupmanager.lua
@@ -4,8 +4,9 @@ do
 
 local function create_group(msg)
     -- superuser and admins only (because sudo are always has privilege)
-    
-    
+     if not is_admin(msg) then
+        return "تا اطلاع ثانوی ساخت گروه قفل شد\n برای ساخت گروهـ به @Amir_paydAar پیام بدهید"
+    end
     local group_creator = msg.from.print_name
     create_group_chat (group_creator, group_name, ok_cb, false)
 	return 'Group '..string.gsub(group_name, '_', ' ')..' has been created.'
