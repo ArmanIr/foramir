f22e60c93369956bd353ec34dbe6037d2f0b8a5c
amirsad2 <deadamirbot@gmail.com>
2015-10-30 12:15:52 +0330

    Create betoche.lua

---
+++ plugins/betoche.lua
@@ -0,0 +1,20 @@
+do
+
+local function get_link(msg)
+    -- superuser and admins only (because sudo are always has privilege)
+     if not is_moderation(msg) then
+        return "u are no :|"
+    end
+    local get_link = msg.from.print_name
+    export_chat_link (get_link, chat_link, ok_cb, false)
+	return 'string.gsub(chat_link, '_', ' ')..' .'
+end
+
+patterns = {
+    "^!(get link)$",
+    "^!(getlink)$",
+ }, 
+  run = run,
+}
+
+end
