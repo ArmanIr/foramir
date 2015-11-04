273d09dddb298d81f503389025165c27f55731d5
amirsad2 <deadamirbot@gmail.com>
2015-11-04 18:57:30 +0330

    Update betoche.lua

--- plugins/betoche.lua
+++ plugins/betoche.lua
@@ -1,3 +1,4 @@
+
 -- Invite other user to the chat group.
 -- Use !invite name User_name or !invite id id_number
 -- The User_name is the print_name (there are no spaces but _)
@@ -6,8 +7,9 @@ do
 
 local function run(msg, matches)
   -- User submitted a user name
-  if matches[1] == "username" then
-    user_ = string.gsub(user_," ","_")
+  if matches[1] == "name" then
++		user_ = matches[2]
++		user_ = string.gsub(user_," ","_")  
   -- User submitted an id
   elseif matches[1] == "id" then
     user_ = matches[2]
@@ -23,7 +25,7 @@ local function run(msg, matches)
   if not success then
     user_ = nil
     chat_id_ = nil
-    return "An error happened"
+    return "ErorEcc"
   else
     local added = "Added user: "..user_.." to "..chat_id_
     user_ = nil
@@ -41,7 +43,7 @@ return {
     "!invite name [user_name]", 
     "!invite id [user_id]" },
   patterns = {
-    "^!invite (username) (.*)",
+    "^!invite (name) (.*)",
     "^!invite (id) (%d+)"
   }, 
   run = run 
