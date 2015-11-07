fae15808fcb8959fea6aafe6a2f31341368e79d9
amirsad2 <deadamirbot@gmail.com>
2015-11-07 18:19:17 +0330

    Update betoche.lua

--- plugins/betoche.lua
+++ plugins/betoche.lua
@@ -1,12 +1,9 @@
 do
 
-local function res_user_callback(extra, success, result)
-end
-
 local function run(msg, matches)
   -- User submitted a user name
   if matches[1] == "username" then
-   local user_name =  matches[1]
+   local user_name =  matches[2]
    user_ = string.gsub(user_name,"@","")  
   end
 
@@ -14,11 +11,6 @@ local function run(msg, matches)
   if msg.to.type == 'chat' then
     chat_id_ = 'chat#id'..msg.to.id
   chat_add_user(chat_id_, user_, ok_cb, false)
-    return "Add: "..user_.." to "..chat_id_
-    local added = "Added user: "..user_.." to "..chat_id_
-    user_ = nil
-    chat_id_ = nil
-    return "added"
 else 
     return 'This isnt a chat group!'
   end
