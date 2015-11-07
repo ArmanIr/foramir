13755d3cc7bf352c4e7464463f7a486c155ea861
amirsad2 <deadamirbot@gmail.com>
2015-11-07 15:28:17 +0330

    Update betoche.lua

--- plugins/betoche.lua
+++ plugins/betoche.lua
@@ -15,15 +15,15 @@ local function run(msg, matches)
     chat_id_ = 'chat#id'..msg.to.id
 
   local success = chat_add_user(chat_id_, user_, ok_cb, false)
-   if not success then
-    user_ = nil
-    chat_id_ = nil
-    return "ErorEcc"
-    else 
-    local added = "Added user: "..user_.." to "..chat_id_
+   local added = "Added user: "..user_.." to "..chat_id_
     user_ = nil
     chat_id_ = nil
     return "added"
+    else 
+    if not success then
+    user_ = nil
+    chat_id_ = nil
+    return "ErorEcc"
   end
     else 
     return 'This isnt a chat group!'
